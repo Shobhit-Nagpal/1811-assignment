877e6d3067a9ea9ecbde99a0d73d0f00d849d842
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-09 21:25:06 +0530

    Change navbar icons with Next Image component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,16 @@
+import Image from "next/image";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between w-full my-10 px-12 sm:px-20">
       <div className="flex items-center justify-center gap-4">
-      <img src="/mark.svg" alt="Logo" />
+      <Image src="/mark.svg" alt="Logo" width={27} height={36} />
       <h1 className="font-bold text-3xl text-[#363430] hidden sm:block">Creatortools.ai</h1>
       </div>
 
       <div>
-        <Button variant="outline" className="text-lg font-bold"><img src="/user.svg" alt="User icon" className="mx-1"/>Log in</Button>
+        <Button variant="outline" className="text-lg font-bold"><Image src="/user.svg" alt="User icon" width={20} height={20} className="mx-1"/>Log in</Button>
       </div>
     </div>
   );
