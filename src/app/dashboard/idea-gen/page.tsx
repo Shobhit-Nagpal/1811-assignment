6af161acad0f0e3677a2d76cf376a91bb5521d79
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-12 01:48:28 +0530

    Add redirect in logo, fix width of dashboard boilerplate pages

--- src/app/dashboard/idea-gen/page.tsx
+++ src/app/dashboard/idea-gen/page.tsx
@@ -1,6 +1,6 @@
 export default function IdeaGeneratorPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full">
+    <div className="flex flex-col items-center justify-center w-3/4">
       <h2 className="text-3xl font-bold">In progress...</h2>
     </div>
   );
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Logo() {
+  const router = useRouter();
+  function handleLogoClick() {
+    router.push("/");
+  }
   return (
-    <div className="flex items-center justify-center gap-4">
+    <div className="flex items-center justify-center gap-4 hover:cursor-pointer" onClick={() => handleLogoClick()}>
       <Image src="/mark.svg" alt="Logo" width={27} height={36} />
       <h1 className="font-bold text-3xl text-[#363430] hidden sm:block">
         Creatortools.ai
