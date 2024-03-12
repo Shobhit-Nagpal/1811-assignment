e8b0aa6c02a6a1f94da695542aba6a7712e75641
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-12 13:58:32 +0530

    Add insertion call to Supabase --commendted

--- src/app/api/generate-post/route.ts
+++ src/app/api/generate-post/route.ts
@@ -1,12 +1,24 @@
+import { createClient } from "@/lib/supabase/server";
 import { NextRequest } from "next/server";
+
+const supabase = createClient();
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-  const { link, tone } = body;
+  const { link, mood } = body;
+
+  //Validate stuff
 
   console.log(body);
 
   //Call to Supabase
+  /*
+  const { data, error } = await supabase
+    .from("linkedin_post_gen")
+    .insert({ link: link, mood: mood })
+    .select();
+  */
   //Call to OpenAI
+  //Call to Supabase
   //Send response
 }
