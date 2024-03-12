f421039d6c8f2b131d17a8b9c6099b8fe8fd1f6c
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-03-12 21:37:42 +0530

    Add Vercel base url

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
